4e0f40a8ef2eb99e59e88bc12195d1b384e2641d
r.deboer <r.deboer@yp-yourpartner.com>
2016-02-17 08:53:42 +0100

    More testing with serialfitting and pump.

--- SewerConsole/Program.cs
+++ SewerConsole/Program.cs
@@ -27,9 +27,9 @@ namespace SewerConsole
             return true;
         }
 
-        static void Main(string[] args)
+        public static void PumpTest1()
         {
-            Pump<TextMessage> pump = new Pump<TextMessage>();            
+            Pump<TextMessage> pump = new Pump<TextMessage>();
             pump.Initialize();
             pump.Interval = 1;
             pump.Start();
@@ -46,13 +46,51 @@ namespace SewerConsole
 
             pump.AddFlow(serial, serial2);
             pump.AddFlow(serial2, serial);
-            
+
             serial.AddInputNotify(new Notify(NotifyOutput));
             serial2.AddInputNotify(new Notify(NotifyOutput2));
 
             serial.Push(new TextMessage("bla bla"));
             serial2.Push(new TextMessage("nib nib"));
+
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            PumpingRound();            
+        }
+
+        public static void PumpingRound()
+        {
+            Pump<TextMessage> pump = new Pump<TextMessage>();
+            pump.Initialize();
+            pump.Interval = 1;
             
+            Pipe<TextMessage> p1 = new Pipe<TextMessage>();
+            Pipe<TextMessage> p2 = new Pipe<TextMessage>();
+            Pipe<TextMessage> p3 = new Pipe<TextMessage>();
+            Pipe<TextMessage> p4 = new Pipe<TextMessage>();
+
+            p1.Initialize();
+            p2.Initialize();
+            p3.Initialize();
+            p4.Initialize();
+
+            pump.AddFlow(p1, p2);
+            pump.AddFlow(p2, p3);
+            pump.AddFlow(p3, p4);
+            pump.AddFlow(p4, p1);
+
+            p1.AddInputNotify(new Notify(NotifyOutput));
+            p2.AddInputNotify(new Notify(NotifyOutput2));
+            p3.AddInputNotify(new Notify(NotifyOutput));
+            p4.AddInputNotify(new Notify(NotifyOutput2));
+            
+            p1.Push(new TextMessage("bla bla"));
+
+            pump.Start();            
+
             Console.ReadLine();
         }
     }
