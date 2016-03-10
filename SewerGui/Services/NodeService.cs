9e39aff37e88e8e3f3d590c1bca9a5287703c0b9
r.deboer <r.deboer@yp-yourpartner.com>
2016-03-10 13:19:12 +0100

    Adding files and stuff to create the xaml interfaces.

---
+++ SewerGui/Services/NodeService.cs
@@ -0,0 +1,38 @@
+﻿using SewerGui.ViewModel;
+using System;
+using System.Collections.ObjectModel;
+
+namespace SewerGui.Services
+{
+    public interface INodeService
+    {
+        ObservableCollection<NodeViewModel> Nodes { get; }
+        Guid CreateNode(NodeViewModel node);
+    }
+
+    public class NodeService : INodeService
+    {
+        public NodeService()
+        {
+            nodes = new ObservableCollection<NodeViewModel>();
+        }
+
+        private ObservableCollection<NodeViewModel> nodes = null;
+
+        public ObservableCollection<NodeViewModel> Nodes
+        {
+            get { return nodes; }                        
+        }
+
+        public Guid CreateNode(NodeViewModel node)
+        {
+            if(node != null)
+            {
+                node.CreateCommand.Execute(null);
+                Nodes.Add(node);
+                return node.UniqueId;
+            }
+            return Guid.Empty;
+        }        
+    }
+}
--- SewerGui/ViewModel/ViewModelLocator.cs
+++ SewerGui/ViewModel/ViewModelLocator.cs
@@ -1,52 +1,18 @@
-﻿/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocatorTemplate xmlns:vm="clr-namespace:SewerGui.ViewModel"
-                                   x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-*/
-
-using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Ioc;
+﻿using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
-using SewerGui.Model;
+using SewerGui.Services;
 
 namespace SewerGui.ViewModel
 {
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// <para>
-    /// See http://www.mvvmlight.net
-    /// </para>
-    /// </summary>
     public class ViewModelLocator
     {
         static ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-
-            if (ViewModelBase.IsInDesignModeStatic)
-            {
-                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
-            }
-            else
-            {
-                SimpleIoc.Default.Register<IDataService, DataService>();
-            }
-
             SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<INodeService, NodeService>();
         }
 
-        /// <summary>
-        /// Gets the Main property.
-        /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
-            "CA1822:MarkMembersAsStatic",
-            Justification = "This non-static member is needed for data binding purposes.")]
         public MainViewModel Main
         {
             get
@@ -55,9 +21,14 @@ namespace SewerGui.ViewModel
             }
         }
 
-        /// <summary>
-        /// Cleans up all the resources.
-        /// </summary>
+        public INodeService Nodes
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<NodeService>();
+            }
+        }
+
         public static void Cleanup()
         {
         }
